ccd0907096a0af64c9f133bfcbd685f3a9060596
johnseetoh25 <yeedingseetoh@gmail.com>
2023-07-17 11:54:01 +0800

    007

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from'react-router-dom';
+import HomePage from './pages/home-page/home-page';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const storedAuthStatus = localStorage.getItem('isAuthenticated');
+    if (storedAuthStatus === 'true') {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage/>} />
+         
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
